2e2f71e413c0f3c07b3ed094d1cee06b0434fe61
arunydvv <arunyadav9704@gmail.com>
2024-12-13 16:54:24 +0530

    Removed unwanted svgs

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+      <>
+          <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+              <nav className='flex justify-between items-center'>
+                  <h1 className='text-3xl'>Foundry</h1>
+              </nav>
+          </header>
+          
+      </>
+  )
+}
